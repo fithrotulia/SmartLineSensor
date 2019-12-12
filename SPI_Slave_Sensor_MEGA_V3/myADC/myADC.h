c51ccf6a96f1ab502552db79a67d795b5eab1b7c
fithrotulia <amalia.irda@gmail.com>
2019-12-13 01:16:24 +0700

    membuat projek smart line sensor for mobile robot

---
+++ SPI_Slave_Sensor_MEGA_V3/myADC/myADC.h
@@ -0,0 +1,11 @@
+
+#ifndef MYADC_MYADC_H_
+#define MYADC_MYADC_H_
+
+#include <avr/io.h>
+
+uint8_t tLow, tHigh;
+void myADC_init(void);
+void myADC_read(uint8_t channel, uint8_t *high, uint8_t *low, uint16_t *full);
+
+#endif /* MYADC_MYADC_H_ */
---
+++ SPI_Slave_Sensor_MEGA_V3/mySPI/mySPI.h
@@ -0,0 +1,20 @@
+
+#ifndef MYSPI_MYSPI_H_
+#define MYSPI_MYSPI_H_
+
+#include <avr/io.h>
+#include <stdio.h>
+
+#define DDR_SPI DDRB
+#define DD_SS DDB0
+#define DD_SCK DDB1
+#define DD_MOSI DDB2
+#define DD_MISO DDB3
+
+//SPI Slave
+void SPI_SlaveInit(void);
+char SPI_SlaveReceive(char cData);
+char SPI_SlavePrint(char *text);
+char SPI_SlavePrintNum(uint16_t number);
+
+#endif /* MYSPI_MYSPI_H_ */
